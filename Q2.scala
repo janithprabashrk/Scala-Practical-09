a6b51106f31a43f2b8564f165b61033eacbeaf73
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-08-19 19:47:40 +0530

    Update Q2.scala

--- Q2.scala
+++ Q2.scala
@@ -1,13 +1,15 @@
-object NumberClassifier {
-  def main(args: Array[String]): Unit = {
-    val input = args(0).toInt
+import scala.io.StdIn.readLine
 
-    val classifyNumber: Int => String = {
+object NumClassifier {
+  def main(args: Array[String]): Unit = {
+    val input = readLine("Please Enter a Numbert : ").toInt
+
+    val classifyNum: Int => String = {
       case n if n <= 0 => "Negative/Zero is input"
       case n if n % 2 == 0 => "Even number is given"
       case _ => "Odd number is given"
     }
 
-    println(classifyNumber(input))
+    println(classifyNum(input))
   }
 }
